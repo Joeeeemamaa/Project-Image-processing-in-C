165cf0796b3044ba7d9e2f51ecd57cc49a2ed0b8
jeremiel1110 <95135287+jeremiel1110@users.noreply.github.com>
2025-03-25 11:19:06 +0100

    edit

--- Img/main.c
+++ Img/main.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+#include "bmp8.h"
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    const char *inputFile = "image.bmp";
+    const char *outputFile = "output.bmp";
+
+    // Load the image
+    t_bmp8 *image = bmp8_loadImage(inputFile);
+    if (!image) {
+        printf("Failed to load image.\n");
+        return 1;
+    }
+
+    // Print image info
+    bmp8_printInfo(image);
+
+    // Save the image
+    bmp8_saveImage(outputFile, image);
+
+    // Free memory
+    bmp8_free(image);
+
     return 0;
 }
